f6d1280235e8d5124d5e896d9b43788a0e3af1df
haonee <s22060@gsm.hs.kr>
2022-09-17 22:42:39 +0900

    다는 못 끝냈지만 할 수 있는데까지 했습니다..!!

---
+++ src/main/java/Repository/AccountRepository.java
@@ -0,0 +1,26 @@
+package Repository;
+
+
+import entity.Account;
+
+import java.util.List;
+
+public interface AccountRepository {
+
+    int count();
+
+    long save(Account account);
+
+    int update(Account account);
+
+    int deleteById(Long id);
+
+    void deleteAll();
+
+    List<Account> findAll();
+
+    Account findById(Long id);
+
+    Account findByEmail(String email);
+}
+
---
+++ src/main/java/entity/Account.java
@@ -0,0 +1,21 @@
+package entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Account {
+    private long id;
+    private String email;
+
+    public Account(long id, String email) {
+        this.id = id;
+        this.email = email;
+
+    }
+    public Account(String email){
+        this.email = email;
+    }
+}
+
